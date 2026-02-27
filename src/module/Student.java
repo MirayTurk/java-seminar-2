118a0484fa8971789d2c1c060f84a28a4e151eb5
MirayTurk <miraylturk@gmail.com>
2026-02-27 15:21:51 +0200

    java seminar 2 class changed name

---
+++ src/module/Student.java
@@ -0,0 +1,33 @@
+package module;
+
+public class Student {
+	private String matriculaNumber;
+	private String name;
+	private String surname;
+	private String faculty;
+	private int birthYear;
+	private Country country;
+	private String passportNumber;
+	//2.getters
+	public String getMatriculaNumber() {
+		return matriculaNumber;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public String getFaculty() {
+		return faculty;
+	}
+    public String getBirthYear() {
+    	return birthYear;
+    }
+    public String getCountry() {
+    	retrun country;
+    }
+    public getPassportNumber() {
+    	return passportNumber;
+    }
+}
