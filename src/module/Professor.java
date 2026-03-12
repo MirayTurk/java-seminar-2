9cdd3d3ebb65696041ec252e386e7376d27c7b90
MirayTurk <miraylturk@gmail.com>
2026-03-12 11:51:42 +0200

    professor

--- src/module/Professor.java
+++ src/module/Professor.java
@@ -2,9 +2,77 @@ package module;
 
 public class Professor {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    private long id;
+    private String name;
+    private String surname ;
+    private ProfDegree degree;
+    
+    private static long counter = 0;
 
+    public long getId() {
+    	return id;
+    }
+    public String getName() {
+    	return name;
+    }
+    public String getSurname() {
+    	return surname;
+    }
+    public ProfDegree getDegree() {
+    	return degree;
+    }
+    
+    public void setId() {
+    	id=counter;
+    	counter++;
+    }
+    
+    public void setName(String inputName) {
+		if( (inputName != null) && (!inputName.isEmpty()) 
+			&& (inputName.matches("[A-Z]{1}[a-z]{2,15}([ ]{1}[A-Z]{1}[a-z]{2,15})?"))) {
+			name = inputName;
+		}
+		else
+		{
+			name = "Unknown";
+		}
+		
 	}
 
-}
+	public void setSurname(String inputSurname) {
+		if( (inputSurname != null) && (!inputSurname.isEmpty()) 
+			&& (inputSurname.matches("[A-Z]{1}[a-z]{2,15}([ ]{1}[A-Z]{1}[a-z]{2,15})?"))) {
+			surname = inputSurname;
+		}
+		else
+		{
+			surname = "Unknown";
+		}
+	}
+	public void setDegree(ProfDegree inputDegree) {
+		if (inputDegree !=null) {
+			degree = ProfDegree.unknown;
+		}
+	}
+	
+	public Professor() {
+		setId();
+		setName("Karina");
+		setSurname("Skirmante");
+		setDegree(ProfDegree.master);
+	}
+	public Professor (String inputName,String inputSurname,ProfDegree inputDegree) {
+		setId();
+		setName(inputName);
+		setSurname(inputSurname);
+		setDegree(inputDegree);
+		
+	}
+	public String toString() {
+	 	String result =id  + " :" + name +  " " + surname + String result =id + ":" + name +  " " + surname +  " (" + degree + ")";
+	 	return result;
+	}
+   }
+
+    
+	
--- src/service/MainService.java
+++ src/service/MainService.java
@@ -5,12 +5,19 @@ import module.Student;
 public class MainService {
 
 	public static void main(String[] args) {
+		System.out.println("----------------STUDENTS--------------------");
 		Student student1 =new Student();
 		System.out.println(student1);
 		
 		Student student2 = new Student (" AB987654" , "John" , "Sarford" ,  "ITF", 2007 , Country.other , "LU236890");
 		
 		System.out.println(student2);
+		
+		System.out.println("---------------PROFESSORS----------------------");
+		Professor prof1= new Professor();
+		System.out.println(prof1);
+		Professor prof2= new Professor("Estera" , "Vitola", ProfDegree.master);
+		System.out.println(prof2);
 	}
 
 }
